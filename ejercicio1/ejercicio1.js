d54b56639ea41650ac67f95d4ebb559a0a339bbc
anacastillo16 <72932228+anacastillo16@users.noreply.github.com>
2025-02-12 18:12:40 +0100

    Comentando el ejercicio 1

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,20 +1,29 @@
+/* Definición de variables necesarias */
 let formulario = document.querySelector("form");
-        let nombre = document.getElementById("name");
-        let precio = document.getElementById("precio");
-        let lista = document.getElementById("lista");
+let nombre = document.getElementById("name");
+let precio = document.getElementById("precio");
+let lista = document.getElementById("lista");
 
-        formulario.addEventListener("submit", (e) => {
-            e.preventDefault();
-            let producto = document.createElement("li");
-            let precioValue = parseFloat(precio.value);
-            producto.textContent = `${nombre.value} - ${precioValue}€`;
-            lista.appendChild(producto);
-            nombre.value = "";
-            precio.value = "";
+/* Función que se ejecutará cuando se envíe el formulario */
+formulario.addEventListener("submit", (e) => {
+    // Evita que se recargue la página
+    e.preventDefault();
+    // Añade el producto a la lista
+    let producto = document.createElement("li");
+    // Obtiene el valor del precio
+    let precioValue = parseFloat(precio.value);
+    // Añade el precio al producto
+    producto.textContent = `${nombre.value} - ${precioValue}€`;
+    // Añade el producto a la lista
+    lista.appendChild(producto);
+    // Resetea el formulario
+    nombre.value = "";
+    precio.value = "";
 
-            if (precioValue> 50) {
-                producto.style.color = "red";
-            } else if (precioValue>= 20 && precioValue <= 50) {
-                producto.style.color = "green";
-            }
-        });
+    // Cambia el color del producto según su precio
+    if (precioValue > 50) {
+        producto.style.color = "red";
+    } else if (precioValue >= 20 && precioValue <= 50) {
+        producto.style.color = "green";
+    }
+});
